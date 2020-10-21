d0d73260c2b0dc3d919777f1c5eb106cb8f78f64
Shinooow <maxence.demougeot1@gmail.com>
2020-10-21 21:14:08 +0200

    test commit item

---
+++ src/main/java/deqo/mdem/mysimplestack/Item.java
@@ -0,0 +1,14 @@
+package deqo.mdem.mysimplestack;
+
+public class Item {
+
+    private String name;
+
+    public Item(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
