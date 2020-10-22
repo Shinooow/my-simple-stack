60099404a614dbe317de04a3be5f1e12d462f1c5
Shinooow <maxence.demougeot1@gmail.com>
2020-10-22 08:01:26 +0200

    unit tests Stack

--- src/test/java/deqo/mdem/mysimplestack/StackTest.java
+++ src/test/java/deqo/mdem/mysimplestack/StackTest.java
@@ -2,6 +2,7 @@ package deqo.mdem.mysimplestack;
 
 import org.junit.Before;
 import org.junit.Test;
+import sun.invoke.empty.Empty;
 
 import static org.junit.Assert.*;
 
@@ -9,29 +10,57 @@ public class StackTest {
     private Item item1;
     private Item item2;
     private Item item3;
+    private Stack stack;
 
     @Before
     public void before(){
-
+        stack = new Stack();
+        item1 = new Item("Item 1");
+        item2 = new Item("Item 2");
+        item3 = new Item("Item 3");
     }
 
     @Test
-    public void isEmpty() {
+    public void isANewStackEmpty() {
+        /* a new stack must be empty */
+        assertEquals("a new stack must be empty", 0, stack.getSize());
+    }
+
+    @Test
+    public void push() throws EmptyStackException{
+        stack.push(item1);
+        assertEquals(item1, stack.peek());
+    }
+
+    @Test
+    public void peek() throws EmptyStackException{
+        stack.push(item1);
+        stack.push(item2);
+        assertEquals(item2, stack.peek());
+    }
+
+    @Test
+    public void pop() throws EmptyStackException{
+        stack.push(item1);
+        stack.push(item2);
+        assertEquals(item2, stack.pop());
+        assertEquals(item1, stack.peek());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void popEmptyStack() throws EmptyStackException {
+        stack.pop();
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void peekEmptyStack() throws EmptyStackException {
+        stack.peek();
     }
 
     @Test
     public void getSize() {
-    }
-
-    @Test
-    public void push() {
-    }
-
-    @Test
-    public void peek() {
-    }
-
-    @Test
-    public void pop() {
+        stack.push(item1);
+        stack.push(item2);
+        assertEquals("the stack size is wrong", 2, stack.getSize());
     }
 }
